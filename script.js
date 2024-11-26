066716115a6101524704ccc2c2a13641f9cf79f3
Chloe-O <chloeosbourne@yahoo.co.uk>
2024-11-26 17:17:35 +0000

    vanilla js, added close nav btn

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-$(".hamburger").on("click", () => {
-  $("#nav1").toggleClass('nav-reveal');
+let hamburger = document.querySelector(".hamburger");
+let btnClose = document.querySelector(".close-btn");
+let nav = document.getElementById("nav");
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("nav-reveal");
+});
+
+btnClose.addEventListener("click", () => {
+  nav.classList.remove("nav-reveal");
 });
